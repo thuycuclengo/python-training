77f569db8975009d0221db378d6895e8a09f8c4f
thuycuclengo <lengothuycuc92@gmail.com>
2025-03-20 15:43:45 +0700

    init commit

---
+++ day1/main.py
@@ -0,0 +1,44 @@
+from json.decoder import scanstring
+
+print('\n< 30 DAYS OF PYTHON | Teclado | Day 1 >\n')
+
+print(100)
+print(10)
+print(3.14, 1.5, 9, sep=" | ", end='$\n')
+
+print(True)
+print(False)
+print("✨🎍🧇🍳🍔")
+# print("line 1")
+# print("line 2")
+# print("line 3")
+
+print("""line 1\t>>>\tDòng thứ 1
+line 2\t>>>\tDòng thứ 2
+line 3\t>>>\tDòng thứ 3\n""")
+"""
+int - 1, 2, 0, -1, -100, ...
+float - 3.14, 7.8, -.9, ...
+str - '', ""
+bool - True/False
+complex - a + bj
+
++ - * / (float) // (int) ** %
+"""
+
+print('[Data types]')
+print('- int -', type(53511))
+print('- float -', type(5.14), '\tLưu ý: 2 dấu / (//) : Trả về kiểu int')
+print('- str (\'\' hoặc "") -', type('Học quài zậy'))
+print('- bool -', type(True))
+print('- complex (số phức) a + bj -', type(3+6j))
+
+# print("""Ngôn ngữ C
+# Ngôn" ngữ C++
+# Ngôn ngữ Java
+# Ngôn ngữ Python""")
+# print(1, 2, 3, 4, sep=" and ", end=' END\n')
+# print(False)
+# print("I'm Cuc")
+# print(type(56+4))
+'Trời ơi'
