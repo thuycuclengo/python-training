8667b3fd3618c5108bcda217f3ad4e4b808a7ca7
thuycuclengo <lengothuycuc92@gmail.com>
2025-03-20 15:57:42 +0700

    second commit

---
+++ day2/main.py
@@ -0,0 +1,12 @@
+"""
+numbers
+int: 48, 316581, 2132, -6430, 0
+float: 3.548, 0.48815, -2.55
+strings
+'Hello', "Python Training", "hi", 'bye'
+boolean: True/False
+complex: a + bj j^2 = -1
+
++ - * / // % **
+print
+"""
