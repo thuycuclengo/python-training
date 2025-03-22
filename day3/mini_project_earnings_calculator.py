30a1ab8fa4af98bce9802161c18c915a9c1700cc
thuycuclengo <lengothuycuc92@gmail.com>
2025-03-22 23:53:15 +0700

    day 3 - mini_project

---
+++ day3/mini_project_earnings_calculator.py
@@ -0,0 +1,5 @@
+name = input("Enter employee's name: ")
+hourly_wage = float(input("Employee's hourly wage: $"))
+hours_worked = int(input("Hours worked: "))
+
+print(f"{name.strip().title()} earned ${hourly_wage * hours_worked:.2f} this week", end=".")
